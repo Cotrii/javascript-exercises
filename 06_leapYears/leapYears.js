a6c223a8e091731e6ca787c852a450679ee9b253
Cotrii <64764789+Cotrii@users.noreply.github.com>
2022-04-24 16:31:56 +0800

    Finished 7 of 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // if (year <= 1900){
+    //     return (year % 4 == 0) && (year % 400 == 0) && (year % 100 == 0);
+    // }
+    // return year % 4 === 0;
+
+    return year % 4 === 0 && ((year % 400 == 0) || (year % 100 !== 0));
 };
 
 // Do not edit below this line
