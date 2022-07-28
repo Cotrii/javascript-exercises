90c97e44c76ebd209d61ccf74e1493716c61bdbf
Cotrii <carlomiguelcortez@gmail.com>
2022-07-28 18:48:48 +0800

    fibo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(fibo) {
 
+    let curr = 1;
+    let prev = 0;
+
+    if (fibo < 0) return "OOPS";
+
+    for (let i = 1; i < fibo; i++){   
+        let temp = curr;
+        curr += prev;
+        prev = temp;
+    }
+
+    //  0+1, ,1+1, 1+2, 2+3, 3+ 5
+    //1, 1,     2,  3 ,, 5, 8
+    return curr;
 };
 
 // Do not edit below this line
