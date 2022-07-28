d6655aa6b376e85bafd9aba565d4bcdb9eda04e1
Cotrii <carlomiguelcortez@gmail.com>
2022-07-28 20:00:14 +0800

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    // if ( ( a.yearOfDeath === undefined)){
+    //     a.yearOfDeath = 2022;
+    // } else if ( a.yearOfDeath === undefined ){
+    //     b.yearOfDeath = 2022;
+    // }
+
+    people.forEach( person => {
+        if (person.yearOfDeath === undefined)
+            person.yearOfDeath = new Date().getFullYear();
+    })
+
+    
+    let arr = people.sort((a, b) => 
+        (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)
+        ? -1 : 1
+    );
+
+    let val = arr[0];
+
+    console.log(arr);
+
+    return val;
+    
 };
 
 // Do not edit below this line
