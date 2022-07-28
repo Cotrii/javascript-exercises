028f9b77d192bf125dea262762d8a712563df1e5
Cotrii <carlomiguelcortez@gmail.com>
2022-07-28 18:25:28 +0800

    9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let revArr = str.split(/[.,!\s]/).reverse();
+    let revArr2 = [];
+
+    for (let i = 0; i < revArr.length; i++) {
+        revArr2[i] = revArr[i].split("").reverse().join("");
+    }
+
+    return (str.split(/[.,!\s]/).join("")).toLowerCase() === revArr2.join("").toLowerCase();
 };
 
 // Do not edit below this line
