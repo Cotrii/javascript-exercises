8c794e75842dc8ec3f1ca47db494b7ef4bf4fa3f
Cotrii <carlomiguelcortez@gmail.com>
2022-07-28 19:26:31 +0800

    Update getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,12 +1,14 @@
 const getTheTitles = function(arr) {
 
-    let arr2 = [];
+    // let arr2 = [];
 
-    for (let i = 0; i < arr.length; i++){
-        arr2[i] = arr[i].title;
-    }
+    // for (let i = 0; i < arr.length; i++){
+    //     arr2[i] = arr[i].title;
+    // }
 
-    return arr2;
+    // return arr2;
+
+    return arr.map(val => `${val.title}`); //map() creates a new array
 
 };
 
